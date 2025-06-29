871f4e075cbe979c5a1997b57903412973998b00
lagithan <vimalrajlagithan@gmail.com>
2025-06-29 10:03:23 +0530

    setup to with nginx config

--- src/main/java/org/example/socialmedia_services/config/SecurityConfig.java
+++ src/main/java/org/example/socialmedia_services/config/SecurityConfig.java
@@ -44,7 +44,7 @@ public class SecurityConfig {
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/auth/login", "/auth/oauth2/**", "/auth/register").permitAll()
+                        .requestMatchers("/auth/login", "/auth/oauth", "/auth/register").permitAll()
                         .anyRequest().authenticated()
                 );
 
