87592d6791183efd2f79bc3d3c59fe3036fe3d8f
lagithan <vimalrajlagithan@gmail.com>
2025-10-03 12:50:18 +0530

    switch to the AWS RDS database

---
+++ src/main/java/org/example/socialmedia_services/services/kafka/kafka_producer.java
@@ -0,0 +1,9 @@
+package org.example.socialmedia_services.services.kafka;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class kafka_producer {
+
+}
